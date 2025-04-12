ecb6aa4d9e6fb2d065826275df84502a4709d2fa
enuabhijeet <guptaabhijeet251@gmail.com>
2025-04-12 19:50:07 +0530

    Add files via upload

---
+++ front.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const diagnosisBtn = document.getElementById('diagnosisBtn');
+    diagnosisBtn.addEventListener('click', function() {
+        window.location.href = 'diagnosis.html';
+    });
+});
